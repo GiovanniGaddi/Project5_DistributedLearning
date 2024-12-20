0752605b20f9587876e6aa34215a37b0ec13078e
s320168 <148537236+s320168@users.noreply.github.com>
2024-12-20 17:38:17 +0100

    Create lenet5.py

---
+++ src/models/lenet5.py
@@ -0,0 +1,29 @@
+from torch import nn
+
+class leNet5(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.feature = nn.Sequential(
+            # first convolutional layer - input 3*32*32 -> output 24*28*28 
+            nn.Conv2d(in_channels=3, out_channels=24, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 24*14*14
+            
+            # second convolutional layer - input 24*14*14 -> output 64*10*10 
+            nn.Conv2d(in_channels=24, out_channels=64, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # 64*5*5
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=64*5*5, out_features=384),
+            nn.ReLU(),
+            nn.Linear(in_features=384, out_features=192),
+            nn.ReLU(),
+            nn.Linear(in_features=192, out_features=100),
+        )
+        
+    def forward(self, x):
+        return self.classifier(self.feature(x))
